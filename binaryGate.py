a5649dfca3afc024315b5390f5c01fa95d63a6a5
Dionisio Tun <sgt.ygdrassil@gmail.com>
2017-03-07 20:39:45 -0600

    Binary Gate Class
    Daughter class of Logic Gate, which will work for AND and OR gates.

---
+++ binaryGate.py
@@ -0,0 +1,16 @@
+class BinaryGate(LogicGate):
+
+    def __init__(self, n):
+
+        LogicGate.__init__(self, n)
+
+        self.pinA = None
+        self.pinB = None
+
+    def getPinA(self):
+
+        return int(input("Enter Pin A input for gate " + self.getLabel() + "-->"))
+
+    def getPinB(self):
+
+        return int(input("Enter Pin B input for gate " + self.getLabel() + "-->"))
